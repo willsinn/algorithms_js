3d44e687f1c4e6379ee628935fc1ce8b1ccdefcf
William Sinn <w.h.sinn@gmail.com>
2020-09-24 17:02:07 -0400

    solved longestCommonPrefix

--- algorithms/string/longestCommonPrefix.js
+++ algorithms/string/longestCommonPrefix.js
@@ -14,3 +14,25 @@
 // Note:
 
 // All given inputs are in lowercase letters a-z.
+
+//For the output to be a string, all elements must have that prefix value.
+var longestCommonPrefix = function (strs) {
+  //With common prefix, we can loop through the first element for like values.
+  let prefix = "";
+
+  if (strs.length > 0) {
+    let firstElem = strs[0];
+    for (let i = 0; i < firstElem.length; i++) {
+      const letter = firstElem[i];
+      for (let j = 1; j < strs.length; j++) {
+        const word = strs[j];
+        const l = word[i];
+        if (l !== letter) {
+          return prefix;
+        }
+      }
+      prefix = prefix.concat(letter);
+    }
+  }
+  return prefix;
+};
