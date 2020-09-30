9a030e95a2f0d4f88de94c0d3907abc369e4f1d4
William Sinn <w.h.sinn@gmail.com>
2020-09-30 03:51:37 -0400

    add nonworkingSolution for bestBuySellStock

---
+++ algorithms/array/bestBuySellStock.js
@@ -0,0 +1,38 @@
+// Say you have an array for which the ith element is the price of a given stock on day i.
+
+// If you were only permitted to complete at most one transaction (i.e., buy one and sell one share of the stock), design an algorithm to find the maximum profit.
+
+// Note that you cannot sell a stock before you buy one.
+
+// Example 1:
+
+// Input: [7,1,5,3,6,4]
+// Output: 5
+// Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+//              Not 7-1 = 6, as selling price needs to be larger than buying price.
+// Example 2:
+
+// Input: [7,6,4,3,1]
+// Output: 0
+// Explanation: In this case, no transaction is done, i.e. max profit = 0.
+
+var maxProfit = function (prices) {
+  let sell = 0;
+  let buy = 0;
+  let index;
+  let profit = 0;
+  for (let i = 0; i < prices.length - 1; i++) {
+    if (buy === 0 || prices[i] < buy) {
+      buy = prices[i];
+      sell = prices[i + 1];
+      index = i;
+    }
+    if (index < i && prices[i] > sell) {
+      sell = prices[i];
+    }
+  }
+  if (sell - buy > 0) {
+    profit = sell - buy;
+  }
+  return profit;
+};
