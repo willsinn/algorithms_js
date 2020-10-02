3212bef8e0be86920f7765879deb15b697dfc01c
William Sinn <w.h.sinn@gmail.com>
2020-10-02 06:29:27 -0400

    add solution

--- data_structures/HashTables/containsDuplicate.js
+++ data_structures/HashTables/containsDuplicate.js
@@ -14,3 +14,21 @@
 
 // Input: [1,1,1,3,3,4,3,2,4,2]
 // Output: true
+
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function (nums) {
+  //With a pointer that declares elem value check if next elem value is equivalent, if so, return true, if not, iterate through array looking for duplicate.
+
+  //1. define upper level for loop as pointer
+  const arr = nums.sort();
+  console.log(arr);
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === arr[i + 1]) return true;
+  }
+  return false;
+
+  //2. define nested loop with depth parameter of 1 to compare values for dupes
+};
