62e8cdd4853b84e50a7376f2ffe99a56fb776d6a
William Sinn <w.h.sinn@gmail.com>
2020-10-02 05:19:54 -0400

    add new DS problem

---
+++ data_structures/BreadthFirstSearch/cousinsInBinaryTree.js
@@ -0,0 +1,11 @@
+// Input: root = [1,2,3,null,4,null,5], x = 5, y = 4
+// Output: true
+// Example 3:
+
+// Input: root = [1,2,3,null,4], x = 2, y = 3
+// Output: false
+
+// Constraints:
+
+// The number of nodes in the tree will be between 2 and 100.
+// Each node has a unique integer value from 1 to 100.
