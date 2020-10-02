9e98e318e29b3fb008fd49c5b15805e82d845c95
William Sinn <w.h.sinn@gmail.com>
2020-10-02 05:49:17 -0400

    add written solution, add partial answer

--- data_structures/BreadthFirstSearch/cousinsInBinaryTree.js
+++ data_structures/BreadthFirstSearch/cousinsInBinaryTree.js
@@ -9,3 +9,33 @@
 
 // The number of nodes in the tree will be between 2 and 100.
 // Each node has a unique integer value from 1 to 100.
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} x
+ * @param {number} y
+ * @return {boolean}
+ */
+var isCousins = function (root, x, y) {
+  //Cousins are values that have the same depth but diff parents, go through both sides of the binary and match the values to return the depth of X & depth of Y.
+  let leftRoot = root;
+  let depth = 0;
+  let leftDepth;
+  while (leftRoot.val !== null) {
+    if (leftRoot.val === x || leftRoot.val === y) {
+      return (leftDepth = depth);
+    }
+    leftRoot = leftRoot.left;
+    depth++;
+  }
+
+  console.log(leftDepth);
+};
