5555de5b10b96f510fe8c8f9e3acfee15b4887e7
William Sinn <w.h.sinn@gmail.com>
2020-10-16 15:26:38 -0400

    add majorElem

---
+++ Algos/Recursion/majorityElement.js
@@ -0,0 +1,38 @@
+// 169. Majority Element
+// Easy
+
+// 3852
+
+// 231
+
+// Add to List
+
+// Share
+// Given an array of size n, find the majority element. The majority element is the element that appears more than ⌊ n/2 ⌋ times.
+
+// You may assume that the array is non-empty and the majority element always exist in the array.
+
+// Example 1:
+
+// Input: [3,2,3]
+// Output: 3
+// Example 2:
+
+// Input: [2,2,1,1,1,2,2]
+// Output: 2
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var majorityElement = function(nums) {
+    let set = new Set(nums)
+    let majorityNum = {};
+    for (let num of set) {
+        const multis = nums.filter(n => n === num).length;
+        if (!majorityNum.multis || majorityNum.multis < multis) {
+        majorityNum = {num, multis};
+        }
+    }
+    return majorityNum.num
+};
