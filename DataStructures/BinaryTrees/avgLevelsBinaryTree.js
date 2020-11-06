edea96d89a515bcc73d18125756d551062dc8c63
William Sinn <w.h.sinn@gmail.com>
2020-11-06 17:49:03 -0500

    save solution note

--- DataStructures/BinaryTrees/avgLevelsBinaryTree.js
+++ DataStructures/BinaryTrees/avgLevelsBinaryTree.js
@@ -21,3 +21,6 @@
 // The average value of nodes on level 0 is 3,  on level 1 is 14.5, and on level 2 is 11. Hence return [3, 14.5, 11].
 // Note:
 // The range of node's value is in the range of 32-bit signed integer.
+
+
+//JS BFS
