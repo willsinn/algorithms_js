d92032bf8d3329db2d0a8b1c9edcf599ae90d966
William Sinn <w.h.sinn@gmail.com>
2021-01-28 21:47:16 -0500

    save notes & solution

--- DataStructures/Tree/symmetricTree.js
+++ DataStructures/Tree/symmetricTree.js
@@ -34,3 +34,24 @@
 // 809,012
 // Submissions
 // 1,687,608
+
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isSymmetric = function(root) {
+    //Base Case
+    if (something) return false;
+    
+    //Return true if right side iteration === left side iteration
+    return (iterate right side of root tree === iterate left side of root tree) 
+};
