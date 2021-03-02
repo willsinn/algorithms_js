eeca94270e8c163717a8185f4771b54ab8a66622
William Sinn <w.h.sinn@gmail.com>
2021-03-02 02:14:12 -0500

    add solution

--- Algos/DepthFirstSearch/binaryTreeTilt.js
+++ Algos/DepthFirstSearch/binaryTreeTilt.js
@@ -52,3 +52,18 @@
 // 111,865
 // Submissions
 // 211,097
+
+
+var findTilt = function(root) {
+    const tilt = { val: 0 };
+    dfs(root, tilt);
+    return tilt.val;
+};
+
+function dfs(root, tilt) {
+    if (!root) return 0;
+    let left = dfs(root.left, tilt);
+    let right = dfs(root.right, tilt);
+    tilt.val += Math.abs(left - right);
+    return root.val + left + right;
+}
