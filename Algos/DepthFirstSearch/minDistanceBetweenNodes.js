f11beab2329304739a4bcb4a882c9a50b7b89d2c
William Sinn <w.h.sinn@gmail.com>
2021-03-01 20:29:50 -0500

    add algo

---
+++ Algos/DepthFirstSearch/minDistanceBetweenNodes.js
@@ -0,0 +1,32 @@
+// 783. Minimum Distance Between BST Nodes
+// Easy
+
+// 949
+
+// 245
+
+// Add to List
+
+// Share
+// Given the root of a Binary Search Tree (BST), return the minimum difference between the values of any two different nodes in the tree.
+
+// Note: This question is the same as 530: https://leetcode.com/problems/minimum-absolute-difference-in-bst/
+
+ 
+
+// Example 1:
+
+
+// Input: root = [4,2,6,1,3]
+// Output: 1
+// Example 2:
+
+
+// Input: root = [1,0,48,null,null,12,49]
+// Output: 1
+ 
+
+// Constraints:
+
+// The number of nodes in the tree is in the range [2, 100].
+// 0 <= Node.val <= 105
