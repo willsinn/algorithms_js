31f7ece82c4467f801446cb108867363b04ce3b2
William Sinn <w.h.sinn@gmail.com>
2021-03-03 09:10:11 -0500

    add failed solution

--- Algos/BreadthFirstSearch/binaryRightSideView.js
+++ Algos/BreadthFirstSearch/binaryRightSideView.js
@@ -35,3 +35,39 @@
 // 414,414
 // Submissions
 // 735,464
+
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var rightSideView = function(root) {
+    if (!root) return [];
+    
+    let result = [];
+    
+    let queue = [];
+    queue.push(root);
+    
+    while (queue.length > 0) {
+        let node = queue.shift();
+        result.push(node.val);
+        
+        if (node.right) {
+            queue.push(node.right)
+        }
+        if (!node.right) {
+            return result;
+        }        
+    }
+    
+    
+};
