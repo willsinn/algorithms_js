1eac9ee0a50c0e36db4cf6942292bba083b1a595
William Sinn <w.h.sinn@gmail.com>
2021-03-09 05:31:15 -0500

    add algo

---
+++ DataStructures/Arrays/maxConsecutiveOnes.js
@@ -0,0 +1,25 @@
+// 485. Max Consecutive Ones
+// Easy
+
+// 1129
+
+// 380
+
+// Add to List
+
+// Share
+// Given a binary array, find the maximum number of consecutive 1s in this array.
+
+// Example 1:
+// Input: [1,1,0,1,1,1]
+// Output: 3
+// Explanation: The first two digits or the last three digits are consecutive 1s.
+//     The maximum number of consecutive 1s is 3.
+// Note:
+
+// The input array will only contain 0 and 1.
+// The length of input array is a positive integer and will not exceed 10,000
+// Accepted
+// 363,051
+// Submissions
+// 687,289
