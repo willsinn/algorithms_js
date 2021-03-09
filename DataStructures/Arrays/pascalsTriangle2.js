ffbd19ba79cc4302027ef7dfff03fcbf11cb922a
William Sinn <w.h.sinn@gmail.com>
2021-03-09 02:39:05 -0500

    add algo

---
+++ DataStructures/Arrays/pascalsTriangle2.js
@@ -0,0 +1,42 @@
+// 119. Pascal's Triangle II
+// Easy
+
+// 1261
+
+// 222
+
+// Add to List
+
+// Share
+// Given an integer rowIndex, return the rowIndexth (0-indexed) row of the Pascal's triangle.
+
+// In Pascal's triangle, each number is the sum of the two numbers directly above it as shown:
+
+
+ 
+
+// Example 1:
+
+// Input: rowIndex = 3
+// Output: [1,3,3,1]
+// Example 2:
+
+// Input: rowIndex = 0
+// Output: [1]
+// Example 3:
+
+// Input: rowIndex = 1
+// Output: [1,1]
+ 
+
+// Constraints:
+
+// 0 <= rowIndex <= 33
+ 
+
+// Follow up: Could you optimize your algorithm to use only O(rowIndex) extra space?
+
+// Accepted
+// 358,803
+// Submissions
+// 686,506
