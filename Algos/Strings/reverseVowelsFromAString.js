c84158bf7342456cc0b11a00ebb0ec755aba8818
William Sinn <w.h.sinn@gmail.com>
2021-03-10 08:48:39 -0500

    add algo

---
+++ Algos/Strings/reverseVowelsFromAString.js
@@ -0,0 +1,29 @@
+// 345. Reverse Vowels of a String
+// Easy
+
+// 949
+
+// 1440
+
+// Add to List
+
+// Share
+// Write a function that takes a string as input and reverse only the vowels of a string.
+
+// Example 1:
+
+// Input: "hello"
+// Output: "holle"
+// Example 2:
+
+// Input: "leetcode"
+// Output: "leotcede"
+// Note:
+// The vowels does not include the letter "y".
+
+ 
+
+// Accepted
+// 266,581
+// Submissions
+// 590,927
