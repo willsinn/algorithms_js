f33ebd674678ed81d04fe58df3a5649225fff470
William Sinn <w.h.sinn@gmail.com>
2021-03-10 05:46:39 -0500

    add algo

---
+++ Algos/Strings/validPalindrome.js
@@ -0,0 +1,34 @@
+// 125. Valid Palindrome
+// Easy
+
+// 1802
+
+// 3639
+
+// Add to List
+
+// Share
+// Given a string s, determine if it is a palindrome, considering only alphanumeric characters and ignoring cases.
+
+ 
+
+// Example 1:
+
+// Input: s = "A man, a plan, a canal: Panama"
+// Output: true
+// Explanation: "amanaplanacanalpanama" is a palindrome.
+// Example 2:
+
+// Input: s = "race a car"
+// Output: false
+// Explanation: "raceacar" is not a palindrome.
+ 
+
+// Constraints:
+
+// 1 <= s.length <= 2 * 105
+// s consists only of printable ASCII characters.
+// Accepted
+// 808,588
+// Submissions
+// 2,110,840
