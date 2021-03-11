82e499423a36397a44de20ea48a3ab8a53453974
William Sinn <w.h.sinn@gmail.com>
2021-03-11 10:04:50 -0500

    add algo

---
+++ Algos/Strings/firstUniqueCharInString.js
@@ -0,0 +1,27 @@
+// 387. First Unique Character in a String
+// Easy
+
+// 2729
+
+// 137
+
+// Add to List
+
+// Share
+// Given a string, find the first non-repeating character in it and return its index. If it doesn't exist, return -1.
+
+// Examples:
+
+// s = "leetcode"
+// return 0.
+
+// s = "loveleetcode"
+// return 2.
+ 
+
+// Note: You may assume the string contains only lowercase English letters.
+
+// Accepted
+// 695,895
+// Submissions
+// 1,293,853
