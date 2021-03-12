ce61e9f782430418bc243f12fa1bb952d85691f7
William Sinn <w.h.sinn@gmail.com>
2021-03-11 22:56:51 -0500

    add algo

---
+++ Algos/Strings/addStrings.js
@@ -0,0 +1,22 @@
+// 415. Add Strings
+// Easy
+
+// 1608
+
+// 366
+
+// Add to List
+
+// Share
+// Given two non-negative integers num1 and num2 represented as string, return the sum of num1 and num2.
+
+// Note:
+
+// The length of both num1 and num2 is < 5100.
+// Both num1 and num2 contains only digits 0-9.
+// Both num1 and num2 does not contain any leading zero.
+// You must not use any built-in BigInteger library or convert the inputs to integer directly.
+// Accepted
+// 271,123
+// Submissions
+// 562,411
