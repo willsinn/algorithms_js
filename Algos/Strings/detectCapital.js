c4d4876d0ed509b7f1d9d2030794c6b596e68b44
William Sinn <w.h.sinn@gmail.com>
2021-03-13 01:21:58 -0500

    save solution

--- Algos/Strings/detectCapital.js
+++ Algos/Strings/detectCapital.js
@@ -36,3 +36,14 @@
 // 186,646
 // Submissions
 // 346,005
+
+/**
+ * @param {string} word
+ * @return {boolean}
+ */
+ var detectCapitalUse = function(word) {
+    if (word === word.toLowerCase()) return true;
+    if (word === word.toUpperCase()) return true;
+    if (word[0] === word[0].toUpperCase() && word.slice(1) === word.slice(1).toLowerCase()) return true
+    return false
+};
