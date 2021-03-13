fdd7296dc2ff2e54e292a998b5b7026452e0d30e
William Sinn <w.h.sinn@gmail.com>
2021-03-13 01:08:41 -0500

    save

--- Algos/Strings/repeatedSubstringPattern.js
+++ Algos/Strings/repeatedSubstringPattern.js
@@ -36,3 +36,11 @@
 // 187,047
 // Submissions
 // 431,503
+
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+ var repeatedSubstringPattern = function(s) {
+    return s.repeat(2).slice(1, -1).includes(s);
+};
