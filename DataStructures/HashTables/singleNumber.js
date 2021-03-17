62f339d7c5a9c0eaff9c853c010d901283cfa300
William Sinn <w.h.sinn@gmail.com>
2021-03-17 01:56:08 -0400

    add solu

--- DataStructures/HashTables/singleNumber.js
+++ DataStructures/HashTables/singleNumber.js
@@ -37,3 +37,27 @@
 // 1,112,971
 // Submissions
 // 1,671,064
+
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+ var singleNumber = function(nums) {
+    let map = new Map();
+    
+    for (let x of nums) {
+        if (map.has(x)) {
+            map.set(x,map.get(x)+1)
+        } else {
+            map.set(x,1)
+        }
+    }
+    
+    for (let [k,v] of map) {
+        if (map.get(k)===1) {
+            return k
+        }
+
+    }
+};
