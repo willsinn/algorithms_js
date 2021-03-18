b6ea3dcce344d589159aa796c587ba8ab674fd1e
William Sinn <w.h.sinn@gmail.com>
2021-03-17 21:58:48 -0400

    add solu

--- Algos/Sorts/largestPerimeterTriangle.js
+++ Algos/Sorts/largestPerimeterTriangle.js
@@ -38,3 +38,20 @@
 // 42,427
 // Submissions
 // 72,402
+
+var largestPerimeter = function(A) {
+    A.sort((a, b) => a - b);
+    let max = 0;
+    for (let i = A.length - 1; i >= 2; i--) {
+        let start = i - 2;
+        let end = i - 1;
+        while (start < end) {
+            if (A[end] + A[start] > A[i]) {
+                return A[end] + A[start] + A[i];
+            } else {
+                start++;
+            }
+        }
+    }
+    return 0;
+};
