267e19ec6713ce31b2737fc3c98ce6ca6270797a
William Sinn <w.h.sinn@gmail.com>
2021-03-24 02:06:49 -0400

    add solution;

--- Algos/Strings/s_wordPattern.js
+++ Algos/Strings/s_wordPattern.js
@@ -44,3 +44,23 @@
 // 249,423
 // Submissions
 // 649,303
+
+var wordPattern = function(pattern, str) {
+    const words = str.split(" ");
+    var map1 = {};
+    var map2 = {};
+    if (pattern.length != words.length) return false;
+    for (let i = 0; i < pattern.length; i++) {
+        if (map1.hasOwnProperty(pattern[i]) && map2.hasOwnProperty(words[i])) {
+            if (map1[pattern[i]] != words[i] || map2[words[i]] != pattern[i]) {
+                return false;
+            }
+        } else if (map1.hasOwnProperty(pattern[i]) || map2.hasOwnProperty(words[i])) {
+            return false;
+        } else {
+            map1[pattern[i]] = words[i];
+            map2[words[i]] = pattern[i];
+        }
+    }
+    return true;
+};
