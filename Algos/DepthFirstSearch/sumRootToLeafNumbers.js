d994aee2d861fad832373e9403e3c5cdade5099b
William Sinn <w.h.sinn@gmail.com>
2021-04-07 22:00:21 -0400

    add solution

--- Algos/DepthFirstSearch/sumRootToLeafNumbers.js
+++ Algos/DepthFirstSearch/sumRootToLeafNumbers.js
@@ -49,3 +49,32 @@
 // 324,333
 // Submissions
 // 632,551
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+ var sumNumbers = function(root) {
+    let result = 0;
+    dfs(root, "");
+    
+    function dfs(node, currentPath){
+        if (!node) return;
+        if (!node.left && !node.right) {
+            currentPath += node.val;
+            result += parseInt(currentPath);
+            return
+        }
+        dfs(node.left, currentPath + node.val);
+        dfs(node.right, currentPath + node.val);
+    }
+    return result
+};
