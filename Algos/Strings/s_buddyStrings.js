46e5f4887aca9b320d63e6204f077d789ac17c1f
William Sinn <w.h.sinn@gmail.com>
2021-04-09 22:17:13 -0400

    add solution & rename

--- Algos/Strings/s_buddyStrings.js
+++ Algos/Strings/s_buddyStrings.js
@@ -43,3 +43,21 @@
 // 88,223
 // Submissions
 // 302,956
+
+/**
+ * @param {string} A
+ * @param {string} B
+ * @return {boolean}
+ */
+ var buddyStrings = function(A, B) {
+    if (A.length != B.length) {
+        return false;
+    }
+    let [dif, chars] = [[], new Set(A)];
+    for (i in A) {
+        if (A[i] != B[i]) {
+            dif.push([A[i], B[i]]);
+        }
+    }
+    return dif.length == 2 && dif[0].join() == dif[1].reverse().join() || (dif.length == 0 && chars.size != A.length);
+};
