968853f12a7ceba3ea764cb1a634603cc5e8e874
William Sinn <w.h.sinn@gmail.com>
2021-04-20 06:14:03 -0400

    add solu

--- Algos/DepthFirstSearch/sumOfLeftLeaves.js
+++ Algos/DepthFirstSearch/sumOfLeftLeaves.js
@@ -32,3 +32,30 @@
 // 242,709
 // Submissions
 // 463,767
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+ var sumOfLeftLeaves = function(node, isLeft = false) {
+
+    if (!node) {
+return 0;
+}
+if (isLeft && !node.left && !node.right) {
+return node.val;
+}
+let sum = 0;
+sum += sumOfLeftLeaves(node.left, true);
+sum += sumOfLeftLeaves(node.right, false);
+return sum;
+
+};
