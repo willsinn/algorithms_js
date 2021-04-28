fc0b8792328371816049b48812d9068c34768d64
William Sinn <w.h.sinn@gmail.com>
2021-04-27 21:59:23 -0400

    add solu

--- Algos/DepthFirstSearch/twoSumInputisBST.js
+++ Algos/DepthFirstSearch/twoSumInputisBST.js
@@ -46,3 +46,29 @@
 // 190,577
 // Submissions
 // 337,818
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} k
+ * @return {boolean}
+ */
+ var findTarget = function(root, k) {
+    let set = new Set();
+    return dfs(root)
+
+    function dfs(node) {
+        if (!node) return false;
+        if (set.has(k - node.val)) return true;
+        set.add(node.val);
+        return dfs(node.left)||dfs(node.right)
+    }
+
+};
