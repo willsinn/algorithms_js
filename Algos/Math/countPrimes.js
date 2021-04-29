c95a1e5df8362f522e369fcb9478ebe3b19b212b
William Sinn <w.h.sinn@gmail.com>
2021-04-29 05:28:20 -0400

    add algo

---
+++ Algos/Math/countPrimes.js
@@ -0,0 +1,36 @@
+// 204. Count Primes
+// Easy
+
+// 2953
+
+// 767
+
+// Add to List
+
+// Share
+// Count the number of prime numbers less than a non-negative number, n.
+
+ 
+
+// Example 1:
+
+// Input: n = 10
+// Output: 4
+// Explanation: There are 4 prime numbers less than 10, they are 2, 3, 5, 7.
+// Example 2:
+
+// Input: n = 0
+// Output: 0
+// Example 3:
+
+// Input: n = 1
+// Output: 0
+ 
+
+// Constraints:
+
+// 0 <= n <= 5 * 106
+// Accepted
+// 462,913
+// Submissions
+// 1,430,683
