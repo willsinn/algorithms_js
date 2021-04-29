4b5fc304e814b1ef1f2cb1acf55bc54f5c87c2c9
William Sinn <w.h.sinn@gmail.com>
2021-04-28 21:47:56 -0400

    add solu

--- Algos/Strings/jewelsAndStones.js
+++ Algos/Strings/jewelsAndStones.js
@@ -33,3 +33,18 @@
 // 619,869
 // Submissions
 // 712,193
+
+/**
+ * @param {string} jewels
+ * @param {string} stones
+ * @return {number}
+ */
+ var numJewelsInStones = function(jewels, stones) {
+    const set = new Set(jewels.split(''));
+    let res = 0;
+    
+    for (let i =0;i<stones.length;i++) {
+        if (set.has(stones[i])) res ++;
+    }
+    return res
+};
