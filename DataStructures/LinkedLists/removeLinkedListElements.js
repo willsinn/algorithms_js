e971084af58c18c129686696d76569c9a8219b3a
William Sinn <w.h.sinn@gmail.com>
2021-04-29 04:58:58 -0400

    add solution

--- DataStructures/LinkedLists/removeLinkedListElements.js
+++ DataStructures/LinkedLists/removeLinkedListElements.js
@@ -37,6 +37,39 @@
 // Submissions
 // 1,140,355
 
-const removeLinkedListElements = () => {
-    
-}
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} val
+ * @return {ListNode}
+ */
+ var removeElements = function(head, val) {
+    let prev = null
+    let node = head;
+    while (node) {
+        if (node.val == val) {
+            if (node === head) {
+                head = head.next;
+                node = head;
+            } else if (prev) {
+                prev.next = node.next
+                node = node.next
+            } 
+        }
+        else {
+            prev = node
+            if (node) {
+                node = node.next
+            }
+        }
+
+    }
+    return head
+
+};
