3bfa3f728a00920dbbf85e568df105886cbffb8a
William Sinn <w.h.sinn@gmail.com>
2021-05-12 08:37:52 -0400

    add solu

--- DataStructures/BinaryTrees/findModeInBST.js
+++ DataStructures/BinaryTrees/findModeInBST.js
@@ -41,3 +41,45 @@
 // 110,082
 // Submissions
 // 250,077
+
+
+const findMode = root => {
+    const handleValue = val => {
+      // handle current count
+      if (val !== currVal) {
+        currVal = val;
+        currCount = 0;
+      }
+      currCount++;
+  
+      if (currCount > maxCount) {
+        // found a new mode
+        maxCount = currCount;
+        modeCount = 1;
+        modes[0] = currVal;
+      } else if (currCount === maxCount) {
+        // found a mode with same count
+        modes[modeCount] = currVal;
+        modeCount++;
+      }
+    };
+  
+    const inorder = node => {
+      if (!node) return;
+  
+      inorder(node.left);
+      handleValue(node.val);
+      inorder(node.right);
+    };
+  
+    let currVal = null;
+    let currCount = 0;
+    let maxCount = 0;
+    let modeCount = 0;
+  
+    const modes = [];
+  
+    inorder(root);
+  
+    return modes.slice(0, modeCount);
+  };
