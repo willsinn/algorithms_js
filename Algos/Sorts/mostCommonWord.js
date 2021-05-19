3113d60eecd7727e1c534ddc9c1655e98088b016
William Sinn <w.h.sinn@gmail.com>
2021-05-18 23:45:42 -0400

    add solution passing all leetcode tests

--- Algos/Sorts/mostCommonWord.js
+++ Algos/Sorts/mostCommonWord.js
@@ -41,3 +41,16 @@
 // 231,804
 // Submissions
 // 509,913
+
+var mostCommonWord = function(paragraph, banned) {
+    //Split the paragraph into an array of words in lowercase
+    const words = paragraph.toLowerCase().split(/\W/);
+    //Create a map to act as histogram of words
+    const mp = Object.create(null);
+    //Filter out empty strings and make the histogram
+    words.filter(x => x).map(x => mp[x] = x in mp ? mp[x] + 1 : 1);
+    //Rather than deleting banned words, just set its value to a negative number
+    banned.map(x => mp[x] = -1)
+    //Return the word with the highest count in the histogram
+    return Object.keys(mp).reduce((a, b) => mp[a] > mp[b] ? a : b);
+};
