fc947f90633a17d06f2766919af57d3b8af5f603
William Sinn <w.h.sinn@gmail.com>
2021-05-19 00:11:28 -0400

    add new algorithm to category folder

---
+++ DataStructures/Arrays/uncommonWordsFromTwoSentences.js
@@ -0,0 +1,39 @@
+// 884. Uncommon Words from Two Sentences
+// Easy
+
+// 616
+
+// 109
+
+// Add to List
+
+// Share
+// We are given two sentences s1 and s2.  (A sentence is a string of space separated words.  Each word consists only of lowercase letters.)
+
+// A word is uncommon if it appears exactly once in one of the sentences, and does not appear in the other sentence.
+
+// Return a list of all uncommon words. 
+
+// You may return the list in any order.
+
+ 
+
+// Example 1:
+
+// Input: s1 = "this apple is sweet", s2 = "this apple is sour"
+// Output: ["sweet","sour"]
+// Example 2:
+
+// Input: s1 = "apple apple", s2 = "banana"
+// Output: ["banana"]
+ 
+
+// Note:
+
+// 0 <= s1.length <= 200
+// 0 <= s2.length <= 200
+// s1 and s2 both contain only spaces and lowercase letters.
+// Accepted
+// 74,899
+// Submissions
+// 116,436
