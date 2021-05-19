c9e97669641a245f0eaa7ed43367328216186cf4
William Sinn <w.h.sinn@gmail.com>
2021-05-19 02:38:34 -0400

    add speed optimized solution

--- Algos/Strings/s_findCommonCharacters.js
+++ Algos/Strings/s_findCommonCharacters.js
@@ -65,10 +65,8 @@
                     const maxRepeated = answerArr >= wordMax ? wordMax : answerArr;
                     const currentRepeated = arr.filter(v => v===letter).length
                     
-                    if (currentRepeated < maxRepeated) arr.push(letter)
-                
+                    if (currentRepeated < maxRepeated) arr.push(letter)   
             }
-            
         }
         res = arr;
         arr = [];
@@ -76,3 +74,16 @@
     }
     return res;
 };
+
+
+var commonChars = function(A) {
+    let res = [...A[0]];
+    for (let i = 1; i < A.length; i++) {
+      res = res.filter(c => {
+        const l = A[i].length;
+        A[i] = A[i].replace(c, "");
+        return l > A[i].length;
+      });
+    }
+    return res;
+  };
