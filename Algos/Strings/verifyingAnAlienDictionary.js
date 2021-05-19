a23603cb055dea63826b6c7592604e5e68089c99
William Sinn <w.h.sinn@gmail.com>
2021-05-19 03:08:37 -0400

    add solution passing all leetcode tests

--- Algos/Strings/verifyingAnAlienDictionary.js
+++ Algos/Strings/verifyingAnAlienDictionary.js
@@ -67,3 +67,23 @@ var isAlienSorted = function(words, order) {
     return isAlien
     
 };
+
+
+
+var isAlienSorted = function(words, order) {
+    for(let i=1;i<words.length;i++) {
+        const a = words[i-1]
+        const b = words[i]
+        const maxLen = Math.max(a.length, b.length)
+        for(let j=0;j<maxLen;j++) {
+            const indexA = order.indexOf(a[j])
+            const indexB = order.indexOf(b[j])
+            if (indexA > indexB) {
+                return false
+            } else if (indexA < indexB) {
+                j=maxLen
+            }
+        }
+    }
+    return true
+};
