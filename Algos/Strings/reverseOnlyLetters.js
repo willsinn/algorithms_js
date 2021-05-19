e41bdf7fe2eff424897a1e92bb886476b0f1e129
William Sinn <w.h.sinn@gmail.com>
2021-05-19 01:38:25 -0400

    add new algorithm to category folder

---
+++ Algos/Strings/reverseOnlyLetters.js
@@ -0,0 +1,37 @@
+// 917. Reverse Only Letters
+// Easy
+
+// 850
+
+// 44
+
+// Add to List
+
+// Share
+// Given a string s, return the "reversed" string where all characters that are not a letter stay in the same place, and all letters reverse their positions.
+
+ 
+
+// Example 1:
+
+// Input: s = "ab-cd"
+// Output: "dc-ba"
+// Example 2:
+
+// Input: s = "a-bC-dEf-ghIj"
+// Output: "j-Ih-gfE-dCba"
+// Example 3:
+
+// Input: s = "Test1ng-Leet=code-Q!"
+// Output: "Qedo1ct-eeLg=ntse-T!"
+ 
+
+// Note:
+
+// s.length <= 100
+// 33 <= s[i].ASCIIcode <= 122 
+// s doesn't contain \ or "
+// Accepted
+// 83,155
+// Submissions
+// 139,809
