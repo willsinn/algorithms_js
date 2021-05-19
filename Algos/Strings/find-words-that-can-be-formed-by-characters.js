d3597b325ff0e8b6a6c1a1f3c5e4e01acf89ec1f
William Sinn <w.h.sinn@gmail.com>
2021-05-19 06:14:28 -0400

    add solution passing all leetcode tests

--- Algos/Strings/find-words-that-can-be-formed-by-characters.js
+++ Algos/Strings/find-words-that-can-be-formed-by-characters.js
@@ -41,3 +41,33 @@
 // 80,905
 // Submissions
 // 119,209
+
+
+/**
+ * @param {string[]} words
+ * @param {string} chars
+ * @return {number}
+ */
+ var countCharacters = function(words, chars) {
+    var res = 0;
+    words.forEach(word=>{
+        var isMatch = true;
+        var tempChars = chars;
+        for(let i = 0;i<word.length; i++) {
+            let l = word[i];
+            var ii = tempChars.indexOf(l);
+            if (ii>-1) {
+                // match 
+                tempChars = tempChars.substring(-1,ii) + tempChars.substring(ii+1,);
+            }else {
+                // not match
+                isMatch = false;
+                break;
+            }
+        }
+        if (isMatch) {
+            res+= word.length;
+        }
+    })
+    return res
+};
