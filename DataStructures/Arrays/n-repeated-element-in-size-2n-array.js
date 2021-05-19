58f5528230d1dd029cdd7636f9f15a90193d6c78
William Sinn <w.h.sinn@gmail.com>
2021-05-19 03:19:02 -0400

    add new algorithm to category folder

--- DataStructures/Arrays/n-repeated-element-in-size-2n-array.js
+++ DataStructures/Arrays/n-repeated-element-in-size-2n-array.js
@@ -1,1 +1,43 @@
 // https://leetcode.com/problems/n-repeated-element-in-size-2n-array/
+
+
+
+// 961. N-Repeated Element in Size 2N Array
+// Easy
+
+// 657
+
+// 265
+
+// Add to List
+
+// Share
+// In a array nums of size 2 * n, there are n + 1 unique elements, and exactly one of these elements is repeated n times.
+
+// Return the element repeated n times.
+
+ 
+
+// Example 1:
+
+// Input: nums[1,2,3,3]
+// Output: 3
+// Example 2:
+
+// Input: nums[2,1,2,5,3,2]
+// Output: 2
+// Example 3:
+
+// Input: nums[5,1,5,2,5,3,5,4]
+// Output: 5
+ 
+
+// Note:
+
+// 4 <= nums.length <= 10000
+// 0 <= nums[i] < 10000
+// nums.length is even
+// Accepted
+// 148,202
+// Submissions
+// 198,217
