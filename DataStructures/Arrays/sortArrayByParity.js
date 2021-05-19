4dbe4730daf242c663257ac01b89794a5a7951cc
William Sinn <w.h.sinn@gmail.com>
2021-05-19 00:42:22 -0400

    add new algorithm to category folder

---
+++ DataStructures/Arrays/sortArrayByParity.js
@@ -0,0 +1,31 @@
+// 905. Sort Array By Parity
+// Easy
+
+// 1734
+
+// 92
+
+// Add to List
+
+// Share
+// Given an array nums of non-negative integers, return an array consisting of all the even elements of nums, followed by all the odd elements of nums.
+
+// You may return any answer array that satisfies this condition.
+
+ 
+
+// Example 1:
+
+// Input: nums = [3,1,2,4]
+// Output: [2,4,3,1]
+// The outputs [4,2,3,1], [2,4,1,3], and [4,2,1,3] would also be accepted.
+ 
+
+// Note:
+
+// 1 <= nums.length <= 5000
+// 0 <= nums[i] <= 5000
+// Accepted
+// 327,565
+// Submissions
+// 436,842
