c31430c5921774041793bf9d80b100d076d23040
William Sinn <w.h.sinn@gmail.com>
2021-05-19 01:38:53 -0400

    add solution passing all leetcode tests

--- Algos/Strings/reverseOnlyLetters.js
+++ Algos/Strings/reverseOnlyLetters.js
@@ -40,3 +40,27 @@
 // 83,155
 // Submissions
 // 139,809
+
+
+/**
+ * @param {string} s
+ * @return {string}
+ */
+/**
+ * @param {string} S
+ * @return {string}
+ */
+ var reverseOnlyLetters = function(S) {
+    if (S.length < 2) return S;
+    const a = S.split('');
+    let i = 0;
+    let j = S.length - 1;
+    while (i < j) {
+      while (!/[a-zA-Z]/.test(S[i])) i++;
+      while (!/[a-zA-Z]/.test(S[j])) j--;
+      if (i < j) [a[i], a[j]] = [a[j], a[i]];
+      i++;
+      j--;
+    }
+    return a.join('');
+  };
